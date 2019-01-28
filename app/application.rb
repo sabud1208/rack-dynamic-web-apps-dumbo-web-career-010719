a34c4d29bcca16b97e299ed8a177e89fbc309f7c
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 02:15:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
     resp.write "#{num_3}\n"
  
     if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+      resp.write "Hello World, You Win"
     else
       resp.write "You Lose"
     end
